f05c2b3114ca7031841ff6d8a2b984caf40d75ed
Andrea Repaci <arepaci@eurosort.com>
2017-12-21 09:12:12 +0100

    Aggiunto Servizio vecchia maniera : WCF

---
+++ WcfServiceLocal/IServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using Core.CustomClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLocal
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di interfaccia "IServiceBase" nel codice e nel file di configurazione contemporaneamente.
+    public interface IServiceBase
+    {
+        bool checkServizioAbilitato(Guid idServizio);
+        boolView checkServizioAbilitato(Core.Utilities.Enum_Servizi Servizio);
+    }
+}
