44e83805fce27290c24a5c0ea44b304466fd9c1c
Andrea Repaci <arepaci@eurosort.com>
2017-12-26 10:40:07 +0100

    Aggiunta pagine di reset e conferma registrazione. Creato metodo per il send mail

--- Ispinia MVC5/Inspinia_MVC5/Controllers/PagesController.cs
+++ Ispinia MVC5/Inspinia_MVC5/Controllers/PagesController.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Core;
+using System.Net.Mail;
+
 namespace Inspinia_MVC5.Controllers
 {
     public class PagesController : Controller
@@ -76,7 +78,61 @@ namespace Inspinia_MVC5.Controllers
         {
             return View();
         }
+
+
+        public ActionResult RegistrationConfirm()
+        {
+            return View();
+        }
+
+        public ActionResult SendMail()
+        {
+            try
+            {
+
+                SmtpClient mySmtpClient = new SmtpClient("my.smtp.exampleserver.net");
+
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                   System.Net.NetworkCredential("username", "password");
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress("test@example.com", "TestFromName");
+                MailAddress to = new MailAddress("test2@example.com", "TestToName");
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
+
+                // add ReplyTo
+                MailAddress replyto = new MailAddress("reply@example.com");
+                myMail.ReplyToList.Add("");
+
+                // set subject and encoding
+                myMail.Subject = "Test message";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // set body-message and encoding
+                myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                  ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("","");
+        }
         
+
         public ActionResult ConfirmRegistration(string nickname, string email, string password)
         {
             Core.FakeService.UserService client = new Core.FakeService.UserService();
@@ -92,7 +148,7 @@ namespace Inspinia_MVC5.Controllers
 
             boolView response = client.AddUsers(newUser);
 
-            return View();
+            return RedirectToAction("RegistrationConfirm", "Pages");
         }
 
 
